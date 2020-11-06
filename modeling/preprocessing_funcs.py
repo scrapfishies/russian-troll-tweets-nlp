2d0ece98b364af4714e21ad150ca16c4b55355fa
Chris Doenlen <scrapfishies@gmail.com>
2020-11-05 17:36:37 -0800

    days work, w2v modeling and stuff

--- modeling/preprocessing_funcs.py
+++ modeling/preprocessing_funcs.py
@@ -46,7 +46,8 @@ def get_emojis(text):
 
 
 def emoji_as_words(emoji_list):
-    emoji_literal = [emoji.demojize(em, delimiters=('', '')) for em in emoji_list]
+    emoji_literal = [emoji.demojize(em, delimiters=('', ''))
+                     for em in emoji_list]
     return emoji_literal
 
 
@@ -72,5 +73,78 @@ def clean_tweet(tweet):
     cleaned_tweet = demojify(remove_punc(
         make_alphanumeric(remove_rt_prefix(remove_url(tweet))))).lower()
     cleaned_tweet = re.sub(r" +", " ", cleaned_tweet).strip()
-    cleaned_tweet = re.sub(r"([^\x00-\x7F])+"," ", cleaned_tweet)
+    cleaned_tweet = re.sub(r"([^\x00-\x7F])+", " ", cleaned_tweet)
     return cleaned_tweet
+
+
+######### HASHTAG DICT #########
+
+hashtag_dict = {'realdonaldtrump': 'real donald trump',
+                'hillaryclinton': 'hillary clinton',
+                'wakeupamerica': 'wake up america',
+                'neverhillary': 'never hillary',
+                'trumpforpresident': 'trump for president',
+                'islamkills': 'islam kills',
+                'blacklivesmatter': 'black lives matter',
+                'survivalguidetothanksgiving': 'survival guide to thanksgiving',
+                'christmasaftermath': 'christmas aftermath',
+                'teaparty': 'tea party',
+                'betteralternativetodebates': 'better alternative to debates',
+                'crookedhillary': 'crooked hillary',
+                'thingsmoretrustedthanhillary': 'things more trusted than hillary',
+                'ruinadinnerinonephrase': 'ruin a dinner in one phrase',
+                'makemehateyouinonephrase': 'make me hate you in one phrase',
+                'idrunforpresidentif': 'id run for president if',
+                'ihavearighttoknow': 'i have a right to know',
+                'hrc': 'hillary clinton',
+                'foxnews': 'fox news',
+                'stopislam': 'stop islam',
+                'icelebratetrumpwith': 'i celebrate trump with',
+                'reasonstoprotest': 'reasons to protest',
+                'trumpsfavoriteheadline': 'trumps favorite headline',
+                'merkelmussbleiben': 'merkel muss bleiben',
+                'rejecteddebatetopics': 'rejected debate topics',
+                'makeamericagreatagain': 'make america great again',
+                'todolistbeforechristmas': 'to',
+                'debatenight': 'debate night',
+                'cruzcrew': 'cruz crew',
+                'tedcruz': 'ted cruz',
+                'draintheswamp': 'drain the swamp',
+                'potuslasttweet': 'potus last tweet',
+                'giftideasforpoliticians': 'gift ideas for politicians',
+                'hillaryshealth': 'hillarys health',
+                'hillaryforprison': 'hillary for prison',
+                'thingspeopleontwitterlike': 'things people on twitter like',
+                'obamaswishlist': 'obamas wish list',
+                'trumpbecause': 'trump because',
+                'nocybercensorship': 'no cyber censorship',
+                'votetrump': 'vote trump',
+                'nevertrump': 'never trump',
+                'americafirst': 'america first',
+                'demdebate': 'dem debate',
+                'gopdebate': 'gop debate',
+                'rednationrising': 'red nation rising',
+                'hillarysemails': 'hillarys emails',
+                'clintonoriginalbirther': 'clinton orginal birther',
+                'trumptrain': 'trump train',
+                'oscarhasnocolor': 'oscar has no color',
+                'clintonoriginalbirther': 'clinton original birther',
+                'basketofdeplorables': 'basket of deplorables',
+                'billclinton': 'bill clinton',
+                'imwithher': 'im with her',
+                'nowplaying': 'now playing',
+                'opiceisis': 'op ice isis',
+                'iceisis': 'ice isis',
+                'tcot': 'top conservatives on twitter',
+                'ccot': 'christian conservatives on twitter',
+                'pjnet': 'patriot journalist network',
+                'donaldtrump': 'donald trump',
+                'newhampshire': 'new hampshire',
+                'trumppence': 'trump pence',
+                'trumpwinsbecause': 'trump wins because',
+                'djt': 'donald j trump',
+                'hillaryrottenclinton': 'hillary rotten clinton',
+                'whereshillary': 'wheres hillary',
+                'dncleak', 'dnc leak',
+                'johnpodesta': 'john podesta'
+                }
